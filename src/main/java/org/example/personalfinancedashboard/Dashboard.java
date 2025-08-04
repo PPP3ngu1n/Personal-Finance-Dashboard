77075b8563abc33b71694b699c30ea29e9665248
Owen Li <owenli886@gmail.com>
2025-08-04 14:20:55 +0100

    Further Testing on JavaFX and how to create a Visible UI

--- src/main/java/org/example/personalfinancedashboard/Dashboard.java
+++ src/main/java/org/example/personalfinancedashboard/Dashboard.java
@@ -2,30 +2,27 @@ package org.example.personalfinancedashboard;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 
 public class Dashboard extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
-        Scene scene = new Scene(root);
+    public void start(Stage Stage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+            Scene scene = new Scene(root);
 
-        primaryStage.setTitle("Personal Finance Dashboard");
-        primaryStage.setFullScreen(true);
-        primaryStage.setFullScreenExitHint("");
+            Stage.setTitle("Personal Finance Dashboard");
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            Stage.setScene(scene);
+            Stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
